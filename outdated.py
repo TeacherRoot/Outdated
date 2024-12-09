e2193f168aa03536366367e1e8bd22c3597292be
Ann Root <ann.root@bvsd.org>
2024-12-09 12:04:04 -0700

    Update outdated.py

--- outdated.py
+++ outdated.py
@@ -1,1 +1,12 @@
 
+months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+
+while True:
+    try:
+        date = input("Input a Date: ")
+        sp = date.rsplit("/")  # Split it into a list 
+        print(sp)
+        # add your code here.  
+        # Check that the length of sp is acutally 3 before assuming the split did something.
+    except:
+        print("there was an error please enter a new date")
